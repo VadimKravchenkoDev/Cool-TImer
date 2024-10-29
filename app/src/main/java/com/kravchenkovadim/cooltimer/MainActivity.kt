525817d6e5fa07c7932d06a3811e310b3c43927e
Arhizmei <arhizmei@gmail.com>
2024-10-29 21:01:28 +0200

    add textView seekbar toolbar button

--- app/src/main/java/com/kravchenkovadim/cooltimer/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/cooltimer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kravchenkovadim.cooltimer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +17,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
